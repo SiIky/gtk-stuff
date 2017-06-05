2f5e4d0f059ce9c62b53b3f6d9f141fbab279296
SiIky <siiky@users.noreply.github.com>
2017-06-06 00:42:37 +0100

    Simplify things & document stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(drop_types_in_const)]
 use std::error::Error;
 use std::io::BufReader;
 
@@ -17,6 +18,9 @@ extern crate try_opt;
 /** Index of the file to play */
 static mut CURRENT: usize = 0;
 
+/** The global state of the program */
+static mut STUFF: Option<Stuff> = None;
+
 lazy_static! {
     /** Files to play */
     static ref TRACKS: Vec<&'static str> = vec![
@@ -29,45 +33,95 @@ lazy_static! {
     ];
 }
 
+fn radio_dummy(button: &RadioButton) {
+    if button.get_active() {
+        /* safe because i ensure all buttons have label */
+        println!("pressionado {}", button.get_label().unwrap());
+    }
+}
+
+fn control_dummy(button: &Button) {
+    /* safe because i ensure all buttons have label */
+    println!("pressionado {}", button.get_label().unwrap());
+}
+
+/** Open and play an audio file */
+fn play_sound(fname: &str) -> Result<(), Box<Error>> {
+    let endpoint: Result<rodio::Endpoint, Box<Error>> =
+        rodio::get_default_endpoint()
+            .ok_or(From::from("Não foi possível abrir dispositivo de som."));
+    let file: std::fs::File = std::fs::File::open(fname)?;
+    let decoder = rodio::Decoder::new(BufReader::new(file))?;
+    rodio::play_raw(&endpoint?, decoder.convert_samples());
+    Ok(())
+}
+
+/** A struct to contain all the stuff */
 #[derive(Debug, Clone)]
 struct Stuff {
+    /** The main window */
     main_window: Window,
+    /** The open option on the file menu */
     file_menu_open: MenuItem,
+    /** The save option on the file menu */
     file_menu_save: MenuItem,
+    /** The quit option on the file menu */
     file_menu_quit: MenuItem,
+    /** The text entry */
     text_entry_name: Entry,
+    /** Phoneme button 1 */
     fonema1: RadioButton,
+    /** Phoneme button 2 */
     fonema2: RadioButton,
+    /** Phoneme button 3 */
     fonema3: RadioButton,
+    /** Phoneme button 4 */
     fonema4: RadioButton,
+    /** Phoneme button 5 */
     fonema5: RadioButton,
+    /** Quality button 1 */
     qualidade1: RadioButton,
+    /** Quality button 2 */
     qualidade2: RadioButton,
+    /** Quality button 3 */
     qualidade3: RadioButton,
+    /** Quality button 4 */
     qualidade4: RadioButton,
+    /** Quality button 5 */
     qualidade5: RadioButton,
+    /** Quality button 6 */
     qualidade6: RadioButton,
+    /** Quality button 7 */
     qualidade7: RadioButton,
+    /** The back button on the main window */
     control_button_back: Button,
+    /** The play button on the main window */
     control_button_play: Button,
+    /** The next button on the main window */
     control_button_next: Button,
+    /** The file loader window */
     file_loader_window: Window,
+    /** The cancel button on the file loader window */
     file_loader_cancel_button: Button,
+    /** The open button on the file loader window */
     file_loader_open_button: Button,
+    /** The file saver window */
     file_saver_window: Window,
+    /** The cancel button on the file saver window */
     file_saver_cancel_button: Button,
+    /** The save button on the file saver window */
     file_saver_save_button: Button,
 }
 
 impl Stuff {
     /** Creates a new `Stuff` with default values */
     fn new() -> Stuff {
-        let top_window = Window::new(gtk::WindowType::Toplevel);
-        let popup_window = Window::new(gtk::WindowType::Popup);
-        let menu_item = MenuItem::new();
-        let entry = Entry::new();
-        let radio_button = RadioButton::new(&[]);
-        let button = Button::new();
+        let button: Button = Button::new();
+        let entry: Entry = Entry::new();
+        let menu_item: MenuItem = MenuItem::new();
+        let popup_window: Window = Window::new(gtk::WindowType::Popup);
+        let radio_button: RadioButton = RadioButton::new(&[]);
+        let top_window: Window = Window::new(gtk::WindowType::Toplevel);
 
         Stuff {
             main_window: top_window.clone(),
@@ -99,8 +153,8 @@ impl Stuff {
         }
     }
 
-    /** Initializes a `Stuff` with the `GLADE_FILE` */
-    fn init(builder: &Builder) -> Option<Stuff> {
+    /** Initializes a `Stuff` */
+    fn init() -> Option<Stuff> {
         fn init_radio_button<F>(builder: &Builder, id: &str, f: F) -> Option<RadioButton>
             where F: Fn(&RadioButton) + 'static
         {
@@ -109,15 +163,29 @@ impl Stuff {
             Some(ret)
         }
 
-        let mut ret = Stuff::new();
-        ret.main_window = try_opt!(builder.get_object("main_window"));
+        /* Load builder from glade file */
+        let builder: Builder = Builder::new_from_string(include_str!("../stuff.glade"));
+        let builder: &Builder = &builder;
 
-        /* init botoes de fonemas */
+        let mut ret: Stuff = Stuff::new();
+
+        /* Main window */
+        {
+            ret.main_window = try_opt!(builder.get_object("main_window"));
+            ret.main_window
+                .connect_delete_event(|_, _| {
+                                          /* Stop the main loop. */
+                                          gtk::main_quit();
+                                          /* Let the default handler destroy the window. */
+                                          Inhibit(false)
+                                      });
+        }
+
+        /* Phoneme buttons */
         {
             fn fonema_button_active(button: &RadioButton) {
                 radio_dummy(button);
             }
-
             ret.fonema1 = try_opt!(init_radio_button(builder, "fonema1", fonema_button_active));
             ret.fonema2 = try_opt!(init_radio_button(builder, "fonema2", fonema_button_active));
             ret.fonema3 = try_opt!(init_radio_button(builder, "fonema3", fonema_button_active));
@@ -125,12 +193,11 @@ impl Stuff {
             ret.fonema5 = try_opt!(init_radio_button(builder, "fonema5", fonema_button_active));
         }
 
-        /* init botoes de qualidade */
+        /* Quality buttons */
         {
             fn qualidade_button_active(button: &RadioButton) {
                 radio_dummy(button);
             }
-
             ret.qualidade1 =
                 try_opt!(init_radio_button(builder, "qualidade1", qualidade_button_active));
             ret.qualidade2 =
@@ -147,7 +214,7 @@ impl Stuff {
                 try_opt!(init_radio_button(builder, "qualidade7", qualidade_button_active));
         }
 
-        /* init botoes de controlo */
+        /* Control buttons */
         {
             fn init_button<F>(builder: &Builder, id: &str, f: F) -> Option<Button>
                 where F: Fn(&Button) + 'static
@@ -156,32 +223,31 @@ impl Stuff {
                 ret.connect_clicked(f);
                 Some(ret)
             }
-
             fn back(button: &Button) {
                 control_dummy(button);
                 unsafe {
                     if CURRENT > 0 {
                         CURRENT -= 1;
                     }
+                    println!("{}", CURRENT);
                 }
             }
-
             fn play(button: &Button) {
                 control_dummy(button);
                 unsafe {
-                    let _ = play_sound(TRACKS[CURRENT]);
+                    println!("{}", TRACKS[CURRENT]);
+                    let _: Result<(), Box<Error>> = play_sound(TRACKS[CURRENT]);
                 }
             }
-
             fn next(button: &Button) {
                 control_dummy(button);
                 unsafe {
                     if CURRENT < TRACKS.len() - 1 {
                         CURRENT += 1;
                     }
+                    println!("{}", CURRENT);
                 }
             }
-
             ret.control_button_back = try_opt!(init_button(builder, "control_button_back", back));
             ret.control_button_play = try_opt!(init_button(builder, "control_button_play", play));
             ret.control_button_next = try_opt!(init_button(builder, "control_button_next", next));
@@ -196,18 +262,24 @@ impl Stuff {
                 ret.connect_activate(f);
                 Some(ret)
             }
-
             fn open(_button: &MenuItem) {
                 println!("open");
+                unsafe {
+                    assert!(STUFF.is_some());
+                    STUFF.as_ref().unwrap().file_loader_window.present();
+                }
             }
             fn save(_button: &MenuItem) {
                 println!("save");
+                unsafe {
+                    assert!(STUFF.is_some());
+                    STUFF.as_ref().unwrap().file_saver_window.present();
+                }
             }
             fn quit(_button: &MenuItem) {
                 println!("quit");
                 gtk::main_quit();
             }
-
             ret.file_menu_open = try_opt!(init_menu_item(builder, "file_menu_open", open));
             ret.file_menu_save = try_opt!(init_menu_item(builder, "file_menu_save", save));
             ret.file_menu_quit = try_opt!(init_menu_item(builder, "file_menu_quit", quit));
@@ -224,66 +296,46 @@ impl Stuff {
             ret.text_entry_name.connect_activate(activate);
         }
 
-        /* open the file chooser windows */
+        /* File chooser windows */
         {
+            /* File loader */
             ret.file_loader_window = try_opt!(builder.get_object("file_loader_window"));
-            ret.file_saver_window =  try_opt!(builder.get_object("file_saver_window"));
+            ret.file_loader_cancel_button =
+                try_opt!(builder.get_object("file_loader_cancel_button"));
+            ret.file_loader_open_button = try_opt!(builder.get_object("file_loader_open_button"));
+            /* File saver */
+            ret.file_saver_window = try_opt!(builder.get_object("file_saver_window"));
+            ret.file_saver_cancel_button = try_opt!(builder.get_object("file_saver_cancel_button"));
+            ret.file_saver_save_button = try_opt!(builder.get_object("file_saver_save_button"));
         }
 
         Some(ret)
     }
 }
 
-fn radio_dummy(button: &RadioButton) {
-    if button.get_active() {
-        /* safe because i ensure all buttons have label */
-        println!("pressionado {}", button.get_label().unwrap());
-    }
-}
-
-fn control_dummy(button: &Button) {
-    /* safe because i ensure all buttons have label */
-    println!("pressionado {}", button.get_label().unwrap());
-}
-
-fn play_sound(fname: &str) -> Result<(), Box<Error>> {
-    let endpoint = rodio::get_default_endpoint();
-    let file = std::fs::File::open(fname)?;
-    let decoder = rodio::Decoder::new(BufReader::new(file))?;
-    if let Some(endpoint) = endpoint {
-        rodio::play_raw(&endpoint, decoder.convert_samples());
-    }
-    Ok(())
-}
-
 fn main() {
-    /* Beginning of `main()` */
+    /** Initialize and open the main window */
+    fn run_gtk() {
+        unsafe {
+            /* Make sure `STUFF` is initialized so it can be used later */
+            STUFF = Stuff::init();
+            match STUFF {
+                None => panic!("Não foi possível abrir a janela."),
+                Some(ref stuff) => {
+                    /* open the main window */
+                    stuff.main_window.show_all();
+                    /* The GTK main loop */
+                    gtk::main();
+                }
+            }
+        }
+    }
+
     if gtk::init().is_err() {
         panic!("Não foi possível inicializar GTK.");
     }
 
-    /* Load builder from glade file */
-    let builder: Builder = Builder::new_from_string(include_str!("../stuff.glade"));
+    run_gtk();
 
-    /* Initialize and open the main window */
-    let stuff = match Stuff::init(&builder) {
-        Some(w) => w,
-        None => panic!("Não foi possível abrir a janela."),
-    };
-
-    /* open the main window */
-    {
-        stuff.main_window.show_all();
-        stuff
-            .main_window
-            .connect_delete_event(|_, _| {
-                                      /* Stop the main loop. */
-                                      gtk::main_quit();
-                                      /* Let the default handler destroy the window. */
-                                      Inhibit(false)
-                                  });
-    }
-
-    /* The GTK main loop */
-    gtk::main();
+    println!("shit");
 }
